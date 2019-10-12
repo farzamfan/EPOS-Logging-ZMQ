cd85ab645a2856f6a22bf6a14054e762f3d0f559
Farzam <farzam.fanni@gmail.com>
2019-10-12 15:02:54 +0200

    synchronize find free port

--- src/main/java/Communication/GatewayServer.java
+++ src/main/java/Communication/GatewayServer.java
@@ -433,28 +433,29 @@ public class GatewayServer {
     }
 
     public int findFreePort() {
-        ServerSocket socket = null;
-        try {
-            socket = new ServerSocket(0);
-            socket.setReuseAddress(true);
-            int port = socket.getLocalPort();
+        synchronized (this) {
+            ServerSocket socket = null;
             try {
-                socket.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            return port;
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        finally {
-            if (socket != null) {
+                socket = new ServerSocket(0);
+                socket.setReuseAddress(true);
+                int port = socket.getLocalPort();
                 try {
                     socket.close();
                 } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                return port;
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                if (socket != null) {
+                    try {
+                        socket.close();
+                    } catch (IOException e) {
+                    }
                 }
             }
+            throw new IllegalStateException("Could not find a free TCP/IP port to start peer on");
         }
-        throw new IllegalStateException("Could not find a free TCP/IP port to start peer on");
     }
 }
